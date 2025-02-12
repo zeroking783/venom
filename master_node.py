35c8b33038e6b7355ee72b38f464d417d5c95970
zeroking783 <bakanov.vladimir2016@gmail.com>
2025-02-12 19:14:11 +0300

    add logging

--- master_node.py
+++ master_node.py
@@ -3,9 +3,26 @@ import os
 from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
 import asyncio
+import logging
+import sys
 import json
 
 load_dotenv()
+
+# Настроим логирование
+LOG_FILE = "app_logs.log"
+
+# Настроим logging для записи логов в файл
+logging.basicConfig(level=logging.DEBUG, 
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[
+                        logging.FileHandler(LOG_FILE),
+                        logging.StreamHandler(sys.stdout)  # Также вывод в консоль
+                    ])
+
+# Перехватываем print для записи в файл
+sys.stdout = open(LOG_FILE, "a")
+sys.stderr = sys.stdout
 
 # Настроим Telegram Bot с aiogram
 BOT_API_TOKEN = os.getenv('BOT_API_TOKEN')  # Токен бота
@@ -33,19 +50,21 @@ def serialize_data(data):
 async def send_to_kafka(message: types.Message):
     user_message = message.text
     username = message.from_user.username
-    print(user_message)
+    logging.info(f"Received message: {user_message} from {username}")  # Логируем входящие сообщения
+
 
     # Формируем сообщение для отправки в Kafka
     kafka_message = serialize_data({"username": username, "message": user_message})
 
     try:
         # Отправляем сообщение в Kafka в топик "telegram_messages"
-        send_message('telegram_messages', kafka_message)
+        send_message('video-download-links', kafka_message)
 
         # Ответим пользователю, что сообщение принято
         await message.reply("Ваше сообщение отправлено в Kafka!")
     except Exception as e:
         # logging.error(f"Error sending message to Kafka: {str(e)}")
+        logging.error(f"Error sending message to Kafka: {str(e)}")
         await message.reply("Произошла ошибка при отправке сообщения в Kafka.")
 
 async def main():
@@ -53,4 +72,5 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
+    logging.info("Starting the bot...")
     asyncio.run(main())
